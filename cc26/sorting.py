08f84605c983e4986144760e7c81b957ca4f7140
abdullah <abdullahshagnoba@gmail.com>
2023-06-12 10:33:51 +0300

    doc: doc string added

--- cc26/sorting.py
+++ cc26/sorting.py
@@ -1,9 +1,10 @@
-"""
+
+def Insert(list,value):
+    """
 takes a sorted list and an input integer and places it on it's correct place
 arguments: sorted list , input integer
 returns : nothing
-"""
-def Insert(list,value):
+    """
     i = 0
     while i < len(list) and value > list[i]:
         i += 1
@@ -15,13 +16,14 @@ def Insert(list,value):
         i += 1 
     list.append(value)
 
-"""
+
+
+def InsertionSort(input):
+    """
 takes a list and sorts it by calling the Insert function
 arguments: list
 returns: sorted list
-"""
-
-def InsertionSort(input):
+    """
     list_two = [0]
     list_two[0] = input[0]
     for i in  range(1,len(input)):
@@ -29,3 +31,4 @@ def InsertionSort(input):
     return list_two
 
 print(InsertionSort([20,18,12,8,5,-2]))
+# InsertionSort()
